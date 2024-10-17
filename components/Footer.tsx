16f2ab3c959a9a5ac799a81dc0096443c6943cfd
Ethan Chey <ethanchey@outlook.com>
2024-10-17 11:34:04 -0400

    footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,14 +18,14 @@ export default function Footer() {
           <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
+          {/*<div>{siteMetadata.author}</div>
+          <div>{` • `}</div>*/}
+          <div>{`© ${new Date().getFullYear()}`} {siteMetadata.author}</div>
           <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
+          <Link className="hover:underline hover:underline-offset-3" href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <p className="text-center">Template from</p>
+          <p className="text-center">Template from:</p>
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog" className="hover:underline hover:underline-offset-3">
             Tailwind Nextjs Theme
           </Link>
